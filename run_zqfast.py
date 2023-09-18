ab6ca66f704ed1f7bfe3f82a5681f12a9d0e93a6
wangquanfugui233 <anhjq3@outlook.com>
2023-09-18 20:34:01 +0800

    aaa

--- run_zqfast.py
+++ run_zqfast.py
@@ -1,6 +1,7 @@
-# 中青快应用
+# 中青快应用 邀请code http://kyy.baertt.com/h5/fastAppWeb/invite/invite_ground_2.html?share_uid=1040776341&channel=c8000
 # author @wangquanfugui233
 # cron 一天一次 一次3毛，自动提现 微信支付宝都可以
+# 已知 青龙2.16.2 python3.10.13以上不能运行，我是2.16.2 python3.10.11
 # 抓userinfo? 只要问号后面的参数就行 
 # 变量    自动提现： 1是微信 0是支付宝，不填不行,多账户就@分开
 # export zqurl='zzzzzz#1@cccccccccc#0'
@@ -39,7 +40,7 @@ def run_so_file(so_file):
             import aiozqfast_310_x86 as zq
             asyncio.run(zq.main())
         else:
-            github_url = f'https://raw.kgithub.com/wangquanfugui233/wool/master/{so_file}'
+            github_url = f'https://raw.fgit.cf/wyourname/wool/master/{so_file}'
             subprocess.run(['curl', '-o', so_file, github_url])
             run_so_file(so_file)
 

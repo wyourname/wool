02fac77afd152be65bce616eea30bd9b9f0d2f30
wyourname <anhjq3@outlook.com>
2023-09-24 13:14:23 +0000

    add aarch64.so

--- run_zqfast.py
+++ run_zqfast.py
@@ -9,40 +9,49 @@
 
 import asyncio
 import platform
-import sys, os, subprocess
+import sys
+import os
+import subprocess
 
-def check_environment():
+
+def check_environment(file_name):
     v, o, a = sys.version_info, platform.system(), platform.machine()
     print(f"Python版本: {v.major}.{v.minor}.{v.micro}, 操作系统类型: {o}, 处理器架构: {a}")
-    if (v.minor in [10]) and o == 'Linux' and a == 'x86_64':
+    if (v.minor in [10]) and o == 'Linux' and a in ['x86_64', 'aarch64', 'armv8-a']:
         print("符合运行要求")
-        return True
+        check_so_file(file_name, v.minor, a)
     else:
         if not (v.minor in [10]):
             print("不符合要求: Python版本不是3.10")
         if o != 'Linux':
             print("不符合要求: 操作系统类型不是Linux")
         if a != 'x86_64':
-            print("不符合要求: 处理器架构不是x86_64")
-        return False
-    
-def check_so_file(filename):
+            print("不符合要求: 处理器架构不是x86_64 aarch64 armv8-a")
+
+
+def check_so_file(filename, py_v, cpu_info):
     if os.path.exists(filename):
         print(f"{filename} 存在")
-        return True
+        import aiozqfast as zq
+        asyncio.run(zq.main())
     else:
-        print(f"{filename} 不存在")
-        return False
-    
-def run_so_file(so_file):
-    if check_environment():
-        if check_so_file(so_file):
-            import aiozqfast_310_x86 as zq
-            asyncio.run(zq.main())
-        else:
-            github_url = f'https://raw.fgit.cf/wyourname/wool/master/other/{so_file}'
-            subprocess.run(['curl', '-o', so_file, github_url])
-            run_so_file(so_file)
+        print(f"{filename} 不存在,前往下载文件")
+        download_so_file(filename, py_v, cpu_info)
+
+
+def download_so_file(filename, py_v, cpu_info):
+    file_base_name = os.path.splitext(filename)[0]
+    if cpu_info in ['aarch64', 'armv8-a']:
+        github_url = f'https://raw.fgit.cf/wyourname/wool/master/other/{file_base_name}_3{py_v}_aarch64.so'
+    if cpu_info == 'x86_64':
+        github_url = f'https://raw.fgit.cf/wyourname/wool/master/other/{file_base_name}_3{py_v}_{cpu_info}.so'
+    # print(github_url)
+    result = subprocess.run(['curl', '-o', filename, github_url])
+    if result.returncode == 0:
+        print(f"下载完成：{filename},调用checl_so_file funtion")
+        check_so_file(filename,py_v,cpu_info)
+    else:
+        print(f"下载失败：{filename}")
 
 if __name__ == '__main__':
-    run_so_file("aiozqfast_310_x86.so")
+    check_environment('aiozqfast.so')

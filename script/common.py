3ec0cbd885fa4103cd6864d565c2b9cd9117f8e8
wfg233 <63834404+wyourname@users.noreply.github.com>
2024-05-13 21:51:23 +0800

    Update common.py

--- script/common.py
+++ script/common.py
@@ -1,7 +1,7 @@
 """
 通用代码
 只需要修改  asyncio.run(common.main("qimao"))  "qimao"代表七猫脚本 "ydcd"代表 有道词典，现有hkread ,speedread,fyys,shengdu,kgyy,qimao
-5.13开放一天
+
 代码请勿用于非法盈利，一切与本人无关，该代码仅用于学习交流，请阅览下载24小时内删除代码
 """
 import asyncio

60b87e01ddc0cfaf3153e8d12a95377753806e16
wfg233 <63834404+wyourname@users.noreply.github.com>
2024-05-26 09:22:37 +0800

    Add files via upload

--- script/bilibili_play.py
+++ script/bilibili_play.py
@@ -1,10 +1,13 @@
 """
 # new Env("bilibili刷播放量")
-# cron: 0 */15 * * * *
+# cron: 1 12 * * *
 通用代码
 只需要修改  asyncio.run(common.main("qimao"))  "qimao"代表七猫脚本 "ydcd"代表 有道词典，现有hkread ,speedread,fyys,shengdu,kgyy,qimao
 代码请勿用于非法盈利，一切与本人无关，该代码仅用于学习交流，请阅览下载24小时内删除代码
 """
+SCRIPT_NAME = "bilibli"   # 脚本名称 记得修改这里
+
+
 import asyncio
 import platform
 import sys
@@ -12,32 +15,34 @@ import os
 import subprocess
 
 
+
 def check_environment(file_name):
     v, o, a = sys.version_info, platform.system(), platform.machine()
     print(f"Python版本: {v.major}.{v.minor}.{v.micro}, 操作系统类型: {o}, 处理器架构: {a}")
     if (v.minor in [10]) and o == 'Linux' and a in ['x86_64', 'aarch64', 'armv8']:
-        print("符合运行要求,arm8没试过不知道行不行,仓库加密的脚本均不支持青龙模块")
+        print("符合运行要求,ARMv7,ARMv8请自行尝试")
         check_so_file(file_name, v.minor, a)
     else:
-        if not (v.minor in [10]):
-            print("不符合要求: Python版本不是3.10")
+        if not (v.minor in [9,10,11,12]):
+            print("不符合要求: Python版本不是3.9 3.10 3.11 3.12中的一种")
             return
         if o != 'Linux':
             print("不符合要求: 操作系统类型不是Linux")
             return
-        if a not in ['x86_64','aarch64']:
-            print("不符合要求: 处理器架构不是x86_64 aarch64 armv8中的一种")
+        if a not in ['x86_64','aarch64', 'armv8', 'armv7']:
+            print("不符合要求: 处理器架构不是x86_64 aarch64 armv8 armv7中的一种,如果你是armv7l,试着在脚本25行修改armv7为armv7l,其他同理")
             return
 
 
 def check_so_file(filename, py_v, cpu_info):
     if os.path.exists(filename):
-        print(f"存在{filename} 文件,继续执行")
+        print(f"{filename} 存在")
         import common
-        asyncio.run(common.main("bilibili"))
+        asyncio.run(common.main(SCRIPT_NAME))
     else:
         print(f"不存在{filename}文件,准备下载文件")
         url = 'https://files.doudoudou.top/?f=/script/others'
+        # 固定 url = 'https://raw.githubusercontent.com/wyourname/wool/master/others'
         download_so_file(filename, py_v, cpu_info,main_url=url)
 
 def run_command(command):
@@ -61,6 +66,8 @@ def download_so_file(filename, py_v, cpu_info, main_url):
         url = main_url + f'/{file_base_name}_3{py_v}_aarch64.so'
     if cpu_info == 'x86_64':
         url = main_url + f'/{file_base_name}_3{py_v}_{cpu_info}.so'
+    if 'armv7' in cpu_info:
+        url = main_url + f'/{file_base_name}_3{py_v}_{cpu_info}.so'
     # print(github_url)
     # 您的命令，使用 -# 参数显示下载进度
     command = ['curl', '-#', '-o', filename, url]
@@ -70,9 +77,7 @@ def download_so_file(filename, py_v, cpu_info, main_url):
         print(f"下载完成：{filename},调用check_so_file funtion")
         check_so_file(filename,py_v,cpu_info)
     else:
-        if main_url != 'https://files.doudoudou.top/?f=/script/others':
-            print(f"下载失败：{filename},更换备用url下载")
-            download_so_file(filename,py_v,cpu_info,main_url='https://files.doudoudou.top/?f=/script/others')
+        print(f"下载失败,请让检查一下他们家网络,或者自行修改为GitHub下载的 url:https://raw.githubusercontent.com/wyourname/wool/master/others")
 
 if __name__ == '__main__':
     check_environment('common.so')

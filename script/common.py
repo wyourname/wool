63f4cfe8cefbf7c807eb082decf0bfabc9f72717
wfg233 <63834404+wyourname@users.noreply.github.com>
2024-07-19 14:55:39 +0800

    Update common.py

--- script/common.py
+++ script/common.py
@@ -3,79 +3,81 @@
 这是一份傻逼代码，使用说明 https://t.me/+04Bmhy0HmmBjMjJl
 代码请勿用于非法盈利，一切与本人无关，该代码仅用于学习交流，请阅览下载24小时内删除代码
 """
-SCRIPT_NAME = "test"   # 脚本名称 记得修改这里
+SCRIPT_NAME = "test"  # 脚本名称 记得修改这里
 
 
+DOWNLOAD_URL = 'https://files.doudoudou.top/?f=/script/others'
+DOWNLOAD_URL2 = 'https://raw.githubusercontent.com/wyourname/wool/master/others'
+import logging
 import asyncio
 import platform
 import sys
 import os
 import subprocess
 
-
+logging.basicConfig(level=logging.INFO)
 
 def check_environment(file_name):
     v, o, a = sys.version_info, platform.system(), platform.machine()
-    print(f"Python版本: {v.major}.{v.minor}.{v.micro}, 操作系统类型: {o}, 处理器架构: {a}")
-    if (v.minor in [9,10,11,12]) and o == 'Linux' and a in ['x86_64', 'aarch64', 'armv8', 'armv7l']:
-        print("符合运行要求,ARMv7,ARMv8请自行尝试")
+    logging.info(f"Python版本: {v.major}.{v.minor}.{v.micro}, 操作系统类型: {o}, 处理器架构: {a}")
+    if (v.minor in [9, 10, 11, 12]) and o == 'Linux' and a in ['x86_64', 'aarch64', 'armv8', 'armv7l']:
+        logging.info("符合运行要求,ARMv7,ARMv8请自行尝试")
         check_so_file(file_name, v.minor, a)
     else:
-        if not (v.minor in [9,10,11,12]):
-            print("不符合要求: Python版本不是3.9 3.10 3.11 3.12中的一种")
+        if not (v.minor in [9, 10, 11, 12]):
+            logging.info("不符合要求: Python版本不是3.9 3.10 3.11 3.12中的一种")
             return
         if o != 'Linux':
-            print("不符合要求: 操作系统类型不是Linux")
+            logging.info("不符合要求: 操作系统类型不是Linux")
             return
-        if a not in ['x86_64','aarch64', 'armv8', 'armv7l']:
-            print("不符合要求: 处理器架构不是x86_64 aarch64 armv8 armv7中的一种")
+        if a not in ['x86_64', 'aarch64', 'armv8', 'armv7l']:
+            logging.info("不符合要求: 处理器架构不是x86_64 aarch64 armv8 armv7中的一种")
             return
-
 
 def check_so_file(filename, py_v, cpu_info):
     if os.path.exists(filename):
-        print(f"{filename} 存在")
+        logging.info(f"本仓库通用文件: {filename}存在")
         import common
         asyncio.run(common.main(SCRIPT_NAME))
     else:
-        print(f"不存在{filename}文件,准备下载文件")
-        url = 'https://files.doudoudou.top/?f=/script/others'
-        # 固定 url = 'https://raw.githubusercontent.com/wyourname/wool/master/others'
-        download_so_file(filename, py_v, cpu_info,main_url=url)
+        logging.info(f"本仓库通用文件: {filename}不在本地,将前往作者仓库拉取")
+        download_so_file(filename, py_v, cpu_info)
 
 def run_command(command):
     process = subprocess.Popen(
         command,
         stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT, 
-        text=True  
+        stderr=subprocess.STDOUT,
+        text=True
     )
+    final_out = ''
     for line in process.stdout:
         line = line.strip()
+        final_out = line
         if "%" in line:
-            print(line)
+            logging.info(line)
+    process.stdout.close()
     process.wait()
-    return process.returncode
+    return process.returncode, final_out
 
-
-def download_so_file(filename, py_v, cpu_info, main_url):
+def download_so_file(filename, py_v, cpu_info):
     file_base_name = os.path.splitext(filename)[0]
     if cpu_info in ['aarch64', 'armv8']:
-        url = main_url + f'/{file_base_name}_3{py_v}_aarch64.so'
-    if cpu_info == 'x86_64':
-        url = main_url + f'/{file_base_name}_3{py_v}_{cpu_info}.so'
-    if 'armv7' in cpu_info:
-        url = main_url + f'/{file_base_name}_3{py_v}_armv7.so'
-    # print(github_url)
-    # 您的命令，使用 -# 参数显示下载进度
-    command = ['curl', '-#', '-o', filename, url]
-    # 执行命令并处理输出
-    result = run_command(command)
-    if result == 0:
-        print(f"下载完成：{filename},调用check_so_file funtion")
-        check_so_file(filename,py_v,cpu_info)
+        url = DOWNLOAD_URL + f'/{file_base_name}_3{py_v}_aarch64.so'
+    elif cpu_info == 'x86_64':
+        url = DOWNLOAD_URL + f'/{file_base_name}_3{py_v}_{cpu_info}.so'
+    elif 'armv7' in cpu_info:
+        url = DOWNLOAD_URL + f'/{file_base_name}_3{py_v}_armv7.so'
+    command = ['curl', '-#', '-o', filename, '-w', '%{http_code}', url]
+    result, stdout = run_command(command)
+    if stdout == '200' and result == 0:
+        logging.info(f"本仓库通用文件下载成功: {filename}")
+        check_so_file(filename, py_v, cpu_info)
     else:
-        print(f"下载失败,请让检查一下他们家网络,或者自行修改为GitHub下载的 url:https://raw.githubusercontent.com/wyourname/wool/master/others")
+        logging.info(f"{filename}下载失败,请手动切换到备用下载url:{DOWNLOAD_URL2} 将DOWNLOAD_URL2改为DOWNLOAD_URL即可")
+        if os.path.exists(filename):
+            os.remove(filename)
 
 if __name__ == '__main__':
     check_environment('common.so')
+

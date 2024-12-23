524fa969bc3298702b129a2aa52d73a5a874ff50
wfg233 <anhjq3@outlook.com>
2024-12-23 19:31:20 +0800

    Update common.py

--- script/common.py
+++ script/common.py
@@ -7,7 +7,7 @@ SCRIPT_NAME = "test"  # 脚本名称 记得修改这里
 
 
 DOWNLOAD_URL2 = 'https://files.doudoudou.top/?f=/script/others'
-DOWNLOAD_URL = 'https://git.doudoudou.top/https://raw.githubusercontent.com/wyourname/wool/master/others'
+DOWNLOAD_URL = 'https://git.kfc50.us.kg/https://raw.githubusercontent.com/wyourname/wool/master/others'
 import logging
 import asyncio
 import platform
